900696f4577b9a9b000355a245aa6e56edc299ba
algorithmicro <algorithmicro@gmail.com>
2019-03-29 18:35:24 +0000

    finished

--- src/utils/checkGameState.js
+++ src/utils/checkGameState.js
@@ -2,7 +2,7 @@ const checkGameState = (word, latestGuess, turn) => {
     if (word !== null && word === latestGuess) {
         return 'Attacker wins';
     }
-    if (turn > 19) {
+    if (turn > 20) {
         return 'Defender wins';
     }
     return 'Game Ready';
